e67e2a6aefd4002b4407aa96ecff8bff425797ed
Harsh3305 <harshverma3305@gmail.com>
2023-05-16 14:27:09 +0530

    Changes done to support dao

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import {getAccount} from "@/service/db_operation";
+import Header from "@/component/header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const account = getAccount();
+  return <div>
+    <Header {...pageProps} account={account} />
+    <Component {...pageProps} account={account} />
+  </div>
 }
